0fd689699fc7bac823e94761d43d774f9b2d2055
hoka-isdl <oka.hayato@mikilab.doshisha.ac.jp>
2024-07-18 11:23:30 +0900

    #19 feat:get_users

--- backend/internal/controller/profile.go
+++ backend/internal/controller/profile.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/ISDL-dev/ISDL-Sentinel/backend/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
--- backend/internal/repository/get_users.go
+++ backend/internal/repository/get_users.go
@@ -10,20 +10,75 @@ func GetUsers(userId int) (userInformation schema.UserInformation, err error) {
 	var avatar schema.UserInformationAvatarListInner
 	var avatarList []schema.UserInformationAvatarListInner
 
-	query :=
-		`SELECT 
+	getUserInfoQuery := `
+		SELECT 
+			user.id AS UserId,
+			user.name AS UserName,
+			user.mail_address AS MailAddress,
+			user.number_of_coin AS NumberOfCoin,
+			status.status_name AS Status,
+			place.place_name AS Place,
+			grade.grade_name AS Grade,
+			avatar.id AS AvatarId,
+			avatar.img_path AS AvatarImgPath
+		FROM 
+			user
+		JOIN 
+			status ON user.status_id = status.id
+		LEFT JOIN 
+			place ON user.place_id = place.id
+		JOIN 
+			grade ON user.grade_id = grade.id
+		LEFT JOIN 
+			user_possession_avatar ON user.id = user_possession_avatar.user_id
+		LEFT JOIN 
+			avatar ON user_possession_avatar.avatar_id = avatar.id
+		WHERE 
+			user.id = ?;`
+	if err := db.QueryRow(
+		getUserInfoQuery,
+		userId,
+	).Scan(
+		&userInformation.UserId,
+		&userInformation.UserName,
+		&userInformation.MailAddress,
+		&userInformation.NumberOfCoin,
+		&userInformation.Status,
+		&userInformation.Place,
+		&userInformation.Grade,
+		&userInformation.AvatarId,
+		&userInformation.AvatarImgPath); err != nil {
+		return nil, fmt.Errorf(
+			"failed to execute a query: %v", err)
+	}
+
+	getMonthStaytimeAndDaysQuery := `
+		SELECT 
+			COUNT(DISTINCT DATE(left_at)) AS AttendanceDays,
+			SEC_TO_TIME(SUM(TIME_TO_SEC(stay_time))) AS StayTime
+		FROM 
+			leaving_history
+		WHERE 
+			user_id = ?
+			AND DATE_FORMAT(left_at, '%Y-%m') = '2024-07';`
+	if err := db.QueryRow(getMonthStaytimeAndDaysQuery, userId).Scan(&userInformation.AttendanceDays, &userInformation.StayTime); err != nil {
+		return nil, fmt.Errorf(
+			"failed to execute a query: %v", err)
+	}
+
+	getAvatarListQuery := `
+		SELECT 
 			avatar.id AS AvatarId,
 			avatar.avatar_name AS AvatarName,
 			avatar.rarity AS Rarity,
-			avatar.img_path AS ImgPath
+			avatar.img_path AS ImgPath 
 		FROM 
-			user_possession_avatar
+			user_possession_avatar 
 		JOIN 
-			avatar ON user_possession_avatar.avatar_id = avatar.id
+			avatar ON user_possession_avatar.avatar_id = avatar.id 
 		WHERE 
-			user_possession_avatar.user_id = ?;
-		`
-	rows, err := db.Query(query, userId)
+			user_possession_avatar.user_id = ?;`
+	rows, err := db.Query(getAvatarListQuery, userId)
 	if err != nil {
 		return nil, fmt.Errorf("getRows db.Query error err:%w", err)
 	}
@@ -34,14 +89,7 @@ func GetUsers(userId int) (userInformation schema.UserInformation, err error) {
 		}
 		avatarList = append(avatarList, avatar)
 	}
-
-	for rows.Next() {
-		err := rows.Scan(&userInformation.UserId, &userInformation.UserName, &userInformation.MailAddress)
-		if err != nil {
-			return nil, fmt.Errorf("getRows rows_title.Scan error err:%w", err)
-		}
-		imagesList = append(imagesList, image)
-	}
+	userInformation.AvatarList = avatarList
 
 	return userInformation, nil
 }
