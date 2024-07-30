2a5361eba94b7b89c37ee5e349fc16828c702215
hoka-isdl <oka.hayato@mikilab.doshisha.ac.jp>
2024-07-30 16:49:08 +0900

    #93 fix: getLabAsistantMemberQuery

--- backend/internal/repositories/lab_asistant_member_repository.go
+++ backend/internal/repositories/lab_asistant_member_repository.go
@@ -17,20 +17,42 @@ func GetLabAsistantMemberRepository() (labAsistantMemberList []schema.GetLabAsis
 		return []schema.GetLabAsistantMember200ResponseInner{}, fmt.Errorf("failed to execute a query to get grade_id: %v", err)
 	}
 
-	getLabAsistantMemberQuery := `SELECT id, name FROM user WHERE grade_id = ?;`
+	getLabAsistantMemberQuery := `
+		SELECT 
+			u.id, 
+			u.name, 
+			u.avatar_id, 
+			a.img_path, 
+			(SELECT COUNT(*) FROM lab_asistant_shift las WHERE las.user_id = u.id) AS count
+		FROM 
+			user u
+		LEFT JOIN 
+			avatar a ON u.avatar_id = a.id
+		WHERE 
+			u.grade_id = ?;
+	`
 	getRows, err := infrastructures.DB.Query(getLabAsistantMemberQuery, gradeId)
 	if err != nil {
 		return []schema.GetLabAsistantMember200ResponseInner{}, fmt.Errorf("getRows getLabAsistantMember Query error err:%w", err)
 	}
+	defer getRows.Close()
+
 	for getRows.Next() {
 		err := getRows.Scan(
 			&labAsistantMember.UserId,
 			&labAsistantMember.UserName,
+			&labAsistantMember.AvatarId,
+			&labAsistantMember.AvatarImgPath,
+			&labAsistantMember.Count,
 		)
 		if err != nil {
 			return []schema.GetLabAsistantMember200ResponseInner{}, fmt.Errorf("getRows getLabAsistantMember Query error err: %v", err)
 		}
 		labAsistantMemberList = append(labAsistantMemberList, labAsistantMember)
+	}
+
+	if err := getRows.Err(); err != nil {
+		return []schema.GetLabAsistantMember200ResponseInner{}, fmt.Errorf("error occurred during iteration: %v", err)
 	}
 
 	return labAsistantMemberList, nil
