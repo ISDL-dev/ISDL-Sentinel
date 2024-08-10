8cac6dacf709e465dbf631ea46dba92fcb0376f6
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-08-10 13:02:17 +0900

    test

---
+++ backend/internal/models/calendar.go
@@ -0,0 +1,29 @@
+package model
+
+type Calendar struct {
+	RoomName     string
+	Summary      string
+	StartDate    string
+	EndDate      string
+	AttendeeMail []string
+}
+
+type CalendarRoomId struct {
+	RoomName   string
+	CalendarId string
+}
+
+const (
+	KC101_LARGE_CALENDAR_ID = "mikilab.doshisha.ac.jp_3739313235333736353437@resource.calendar.google.com"
+	KC103_CALENDAR_ID       = "mikilab.doshisha.ac.jp_33353234353936362d333132@resource.calendar.google.com"
+	KC111_CALENDAR_ID       = "mikilab.doshisha.ac.jp_3235333239333534343633@resource.calendar.google.com"
+	KC116_CALENDAR_ID       = "mikilab.doshisha.ac.jp_38363338373137302d343939@resource.calendar.google.com"
+	KC119_CALENDAR_ID       = "mikilab.doshisha.ac.jp_38363935323038372d393739@resource.calendar.google.com"
+)
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+	Expiry       string `json:"expiry"`
+}
--- backend/internal/services/attendees_list_service.go
+++ backend/internal/services/attendees_list_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"fmt"
 
+	"github.com/ISDL-dev/ISDL-Sentinel/backend/internal/infrastructures"
 	"github.com/ISDL-dev/ISDL-Sentinel/backend/internal/repositories"
 	"github.com/ISDL-dev/ISDL-Sentinel/backend/internal/schema"
 )
@@ -12,6 +13,7 @@ func GetAttendeesListService() (attendeeList []schema.GetAttendeesList200Respons
 	if err != nil {
 		return []schema.GetAttendeesList200ResponseInner{}, fmt.Errorf("failed to execute query to get in room status id: %v", err)
 	}
+	infrastructures.GetCalendarList()
 	attendeeList, err = repositories.GetInRoomUserListRepository(inRoomStatusId)
 	if err != nil {
 		return []schema.GetAttendeesList200ResponseInner{}, fmt.Errorf("failed to execute query to get in room user list: %v", err)
