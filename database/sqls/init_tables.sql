a396d9b3accd4a51d7a174915539c92322181391
akiya125 <akiya2012.2012@icloud.com>
2024-08-27 22:20:58 +0900

    #45 fix: digest auth

--- database/sqls/init_tables.sql
+++ database/sqls/init_tables.sql
@@ -88,6 +88,7 @@ CREATE TABLE IF NOT EXISTS entering_history(
 CREATE TABLE IF NOT EXISTS leaving_history(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNSIGNED NOT NULL,
+    entering_history_id INT UNSIGNED NOT NULL,
     left_at DATETIME NOT NULL,
     stay_time TIME NOT NULL,
     is_last_leaving Boolean NOT NULL,
