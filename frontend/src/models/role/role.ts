b43b03799c28590ac25793bddf86e700574522ad
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-10-13 14:11:31 +0900

    再度修正

--- frontend/src/models/role/role.ts
+++ frontend/src/models/role/role.ts
@@ -3,8 +3,8 @@ import { AuthUser } from "../../userContext";
 export const CHIEF = "チーフ";
 export const INFRA = "インフラ";
 export const isChief = (authUser?: AuthUser) => {
-  return authUser && (authUser.role_list.includes(CHIEF) ?? []);
+  return authUser && (authUser.role_list?.includes(CHIEF) ?? []);
 };
 export const isInfra = (authUser?: AuthUser) => {
-  return authUser && (authUser.role_list.includes(INFRA) ?? []);
+  return authUser && (authUser.role_list?.includes(INFRA) ?? []);
 };
