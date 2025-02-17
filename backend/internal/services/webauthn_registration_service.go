8d72d64bc7b763eee6feccd08eaa0871adbe3fb0
hoka-isdl <oka.hayato@mikilab.doshisha.ac.jp>
2025-02-17 15:14:14 +0900

    fix

--- backend/internal/services/webauthn_registration_service.go
+++ backend/internal/services/webauthn_registration_service.go
@@ -39,7 +39,7 @@ func GetBeginRegistrationService(userName string, w http.ResponseWriter, r *http
 		RPOrigin:      rpOrigin,
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create WebAuthn from config: %w%w", err, rpID)
+		return nil, fmt.Errorf("failed to create WebAuthn from config: %w:%s:%s", err, serverHost, rpID)
 	}
 
 	userCredential, err := repositories.GetUserCredential(userName)
